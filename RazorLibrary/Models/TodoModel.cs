b06c7d49694bf2dd429eab46200115d91557206b
Eduardo Arruda <eduhza@gmail.com>
2023-04-19 00:04:54 -0300

    first commit

---
+++ RazorLibrary/Models/TodoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorLibrary.Models;
+
+public class TodoModel
+{
+    public int Id { get; set; }
+    public string TodoItem { get; set; }
+    public bool IsComplete { get; set; }
+}
+
